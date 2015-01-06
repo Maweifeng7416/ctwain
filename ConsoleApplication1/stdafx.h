79b636517713e71a096e00e702559aa657638360
soukoku <yinchun.wang@gmail.com>
2015-01-05 22:45:19 -0500

    Added console project for testing internal msg loop.

--- ConsoleApplication1/stdafx.h
+++ ConsoleApplication1/stdafx.h
@@ -11,11 +11,9 @@
 // Windows Header Files:
 #include <windows.h>
 
-// C RunTime Header Files
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
+#include <stdio.h>
 #include <tchar.h>
+
 
 
 // TODO: reference additional headers your program requires here
